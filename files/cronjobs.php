76ba501ebdc58d1c80897d5db59031dd85eaf30e
Sebastian Öttl <sebastian.oettl@wcfsolutions.com>
2011-12-03 11:30:41 +0100

    Added copyright notice to cronjobs.php file

--- files/cronjobs.php
+++ files/cronjobs.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * @author	Sebastian Oettl
+ * @copyright	2011 Sebastian Oettl <http://www.infinite-site.org/>
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
 try {
 	// include wcf/wsis
 	require_once('./global.php');	
